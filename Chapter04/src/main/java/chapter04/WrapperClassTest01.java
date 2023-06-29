446bb11a80bc058b83b5eaf4d6f585c09272817a
leesu <leesu@BOOK-2NES7KOICT>
2023-06-29 14:53:58 +0900

    Wrapper class commit

--- Chapter04/src/main/java/chapter04/WrapperClassTest01.java
+++ Chapter04/src/main/java/chapter04/WrapperClassTest01.java
@@ -3,8 +3,21 @@ package chapter04;
 public class WrapperClassTest01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// deprecated : 조만간 없어질거다 쓰지마라
+		Integer i = new Integer(10);
+		
+		// Auto Boxing : 자동으로 객체 생성해줌
+		Integer i2 = 10;
+		Long l = 10L;
+		
+		System.out.println(i2.equals(10));	// 여기도 Auto Boxing
+		
+		System.out.println(i2 == 10);	// 이건 Auto Boxing이 아님, 이건 i2를 Auto Unboxing해서 상수로 비교한 것.
+//		System.out.println(i2.intValue() == 10);
+		
+		int m = i2 + 10;	// 여기도 Auto UnBoxing
+//		int m = i2.intValue() + 10;
+		
 	}
 
 }
